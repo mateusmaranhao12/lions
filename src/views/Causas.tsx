848ca874856c6d42205ab2167cebecc941f09ffd
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-04-28 10:47:07 -0300

    Implement slug

--- src/views/Causas.tsx
+++ src/views/Causas.tsx
@@ -1,18 +1,19 @@
 'use client'
 
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Causas() {
     const causas = [
-        { nome: "Juventude de LCIF", icone: "/assets/causas1.png" },
-        { nome: "Esforços Humanitários", icone: "/assets/causas2.png" },
-        { nome: "Câncer", icone: "/assets/causas3.png" },
-        { nome: "Socorro em Catástrofes", icone: "/assets/causas4.png" },
-        { nome: "Diabetes", icone: "/assets/causas5.png" },
-        { nome: "Fome", icone: "/assets/causas6.png" },
-        { nome: "Meio ambiente", icone: "/assets/causas7.png" },
-        { nome: "Visão", icone: "/assets/causas8.png" },
-    ];
+        { nome: 'Juventude de LCIF', slug: 'juventude', icone: '/assets/causas1.png' },
+        { nome: 'Esforços Humanitários', slug: 'humanitarios', icone: '/assets/causas2.png' },
+        { nome: 'Câncer', slug: 'cancer', icone: '/assets/causas3.png' },
+        { nome: 'Socorro em Catástrofes', slug: 'catastrofes', icone: '/assets/causas4.png' },
+        { nome: 'Diabetes', slug: 'diabetes', icone: '/assets/causas5.png' },
+        { nome: 'Fome', slug: 'fome', icone: '/assets/causas6.png' },
+        { nome: 'Meio ambiente', slug: 'meio-ambiente', icone: '/assets/causas7.png' },
+        { nome: 'Visão', slug: 'visao', icone: '/assets/causas8.png' },
+    ]
 
     return (
         <div className="p-10 text-white">
@@ -20,7 +21,7 @@ export default function Causas() {
 
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 {causas.map((causa, index) => (
-                    <div key={index} className="bg-violet-700 cursor-pointer rounded-xl flex flex-col items-center justify-center p-6 hover:bg-violet-900 transition">
+                    <Link key={index} href={`/causas/${causa.slug}`} className="bg-violet-700 cursor-pointer rounded-xl flex flex-col items-center justify-center p-6 hover:bg-violet-900 transition">
                         <Image
                             src={causa.icone}
                             alt={causa.nome}
@@ -29,7 +30,7 @@ export default function Causas() {
                             className="mb-4"
                         />
                         <h2 className="text-lg font-semibold text-center">{causa.nome}</h2>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
