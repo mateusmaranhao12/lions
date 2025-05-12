a3260eb92fe125f33cda2dce5b26ce009dd6d1c7
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-05-11 21:45:26 -0300

    Update doacoes

--- src/app/doacoes/page.tsx
+++ src/app/doacoes/page.tsx
@@ -5,18 +5,18 @@ import Link from "next/link"
 
 const DoacoesPage = () => {
     const [showQR, setShowQR] = useState(false)
-    const chavePix = "mateusnmaranhao@gmail.com"
+    const chavePix = "48.387.499/0001-68"
+    const chaveCopiarColar = "00020126360014br.gov.bcb.pix0114483874990001685204000053039865802BR5910----------6007ARACAJU62070503***63042AE0"
     const [copiado, setCopiado] = useState(false)
 
     const copiarPix = () => {
-        navigator.clipboard.writeText(chavePix)
+        navigator.clipboard.writeText(chaveCopiarColar)
         setCopiado(true)
         setTimeout(() => setCopiado(false), 3000)
     }
 
     return (
-        <main className="min-h-screen flex flex-col md:items-center md:justify-center px-4 py-8">
-            <h1 className="text-6xl font-bold text-center mb-6 text-white">Como você deseja doar?</h1>
+        <main className="min-h-screen flex flex-col md:items-center px-4 py-8">
             <p className="text-justify text-sm md:text-base w-full max-w-5xl mx-auto text-white mb-8">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Proin lacinia volutpat condimentum. Vivamus faucibus risus dolor,
@@ -28,6 +28,7 @@ const DoacoesPage = () => {
                 consectetur adipiscing elit. Duis tristique pharetra dictum. Nullam id magna cursus, consequat sem vel,
                 iaculis dui. Quisque eu augue id ipsum tristique dapibus id in sapien.
                 In iaculis euismod enim id dapibus.</p>
+            <h1 className="text-6xl font-bold text-center mb-6 text-white">Como você deseja doar?</h1>
 
             <div className="flex flex-col md:flex-row gap-6">
                 <button
@@ -50,17 +51,17 @@ const DoacoesPage = () => {
                 <div className="mt-10 bg-white p-6 rounded-lg shadow-lg text-center max-w-sm w-full">
                     <h2 className="text-xl font-semibold text-black mb-4">Doe via PIX</h2>
                     <Image
-                        src="/assets/qrcode.svg"
+                        src="/assets/qrcode.png"
                         alt="QR Code PIX"
-                        width={200}
-                        height={200}
+                        width={500}
+                        height={500}
                         className="mx-auto"
                     />
                     <p className="text-black mt-4">Chave PIX:</p>
                     <p className="text-black font-semibold">{chavePix}</p>
 
                     {copiado && (
-                        <span className="text-green-600 font-medium text-sm mt-2 block">
+                        <span className="cursor-pointer text-green-600 font-medium text-sm mt-2 block">
                             Chave PIX copiada!
                         </span>
                     )}
@@ -69,7 +70,7 @@ const DoacoesPage = () => {
                         onClick={copiarPix}
                         className="mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-500 w-full"
                     >
-                        Copiar chave
+                        Copiar QRCode
                     </button>
                 </div>
             )}
