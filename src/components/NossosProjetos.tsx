e6a554061604c3775ca982ed06b0f9e46ac948c6
wilia <wilia.francisco@souunit.com.br>
2025-05-12 14:09:18 -0300

    Tamanho automatico

--- src/components/NossosProjetos.tsx
+++ src/components/NossosProjetos.tsx
@@ -84,7 +84,7 @@ export default function NossosProjetos() {
                 {/* Imagem com setas */}
                 <div className="flex flex-col items-center mb-4">
                     {/* Container fixo da imagem + setas */}
-                    <div className="relative w-full max-w-6xl h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px] rounded-xl overflow-hidden shadow-xl">
+                    <div className="relative w-full max-w-6xl aspect-[12/7] rounded-xl overflow-hidden shadow-xl">
                     <Image
                             src={projeto.imagem}
                             alt={projeto.titulo}
